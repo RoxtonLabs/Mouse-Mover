9775a9c56024440f75f8dcb12b327002f1e4e9f6
RoxtonLabs <roxtonlabs@gmail.com>
2015-12-14 17:06:37 +0000

    First draft complete. Timings are hard-coded for now; icon needs to be less bad.

---
+++ MouseMover/MouseMover/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MouseMover
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startButton = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(117, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Click to restore Mouse Mover";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 47);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainForm";
+            this.Text = "Mouse Mover";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ MouseMover/MouseMover/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Runtime.InteropServices;
+
+namespace MouseMover
+{
+    public partial class MainForm : Form
+    {
+        //Import the LastInputInfo function
+        [DllImport("User32.dll")]
+        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+
+        //Import the SendInput function
+        [DllImport("user32.dll")]
+        private static extern uint SendInput(uint nInputs, InputProvider[] pInputs, int cbSize);
+
+        //A brace of structures for holding input:
+        //Mouse-movement based on code from http://csharp-tricks-en.blogspot.co.uk/2011/07/control-mouse.html
+        struct MouseInput   
+        {
+            public int X;
+            public int Y;
+            public uint mouseData;
+            public uint mouseFlag;  //whether it's a wheel, button press, movement, etc.
+            public uint time; // time of the event
+            public IntPtr extraInfo; // further information
+        }
+
+        struct InputProvider    //Feeds the data to the SendInput method
+        {
+            public int type; // type of the input, 0 for mouse  
+            public MouseInput data; // mouse data
+        }
+
+        bool running = false;
+        static uint GetLastInputTime()
+        {   //Calculates the time since last input in seconds
+            //Sourced from http://www.pinvoke.net/default.aspx/user32.GetLastInputInfo
+            uint idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            uint envTicks = (uint)Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                uint lastInputTick = lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+            }
+            return ((idleTime > 0) ? (idleTime / 1000) : 0);
+        }
+
+        System.Timers.Timer watchTimer = new System.Timers.Timer(60000);    //For now just check once a minute. Maybe later make this user-configurable
+        public MainForm()
+        {
+            InitializeComponent();
+            watchTimer.Enabled = false;
+            watchTimer.Elapsed += new ElapsedEventHandler(watchTimer_Elapsed);
+        }
+
+        void watchTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {   //When the timer expires, check the time since the last input
+            if (GetLastInputTime() > 60)    //Basically means we'll only move once every two minutes; that's fine
+            {
+                Console.WriteLine("User has been idle for " + GetLastInputTime().ToString() + " seconds.");
+                
+                //Move the mouse slightly
+                MouseInput movement = new MouseInput();
+                movement.X = 0;
+                movement.Y = 0;
+                movement.mouseData = 0;
+                movement.time = 0;
+                movement.mouseFlag = 0x8000 | 0x0001; //Those are constants for absolute screen position and "mouse move" respectively
+                InputProvider[] MouseEvent = new InputProvider[1];
+                MouseEvent[0].type = 0;
+                MouseEvent[0].data = movement;
+                SendInput((uint)MouseEvent.Length, MouseEvent, Marshal.SizeOf(MouseEvent[0].GetType()));
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {   //Start or stop the monitoring
+            if (!running)
+            {
+                watchTimer.Enabled = true;
+                watchTimer.Stop();
+                watchTimer.Start();
+                startButton.Text = "Stop";
+                running = true;
+            }
+            else
+            {
+                watchTimer.Enabled = false;
+                watchTimer.Stop();
+                startButton.Text="Start";
+                running = false;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {   //Hide this in the system tray instead of in the taskbar
+            if (WindowState==FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                Hide();
+            }
+            else
+            {
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
